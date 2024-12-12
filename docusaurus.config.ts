275c564bb3c46bf9759862b2369f688fe197c3f3
coda <coda@polyhedra.network>
2024-12-12 19:32:27 +0800

    fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -48,6 +48,12 @@ const config: Config = {
       } satisfies Preset.Options,
     ],
   ],
+  stylesheets: [
+    {
+      href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap",
+      type: "text/css",
+    },
+  ],
   themeConfig: {
     metadata: [
       {
