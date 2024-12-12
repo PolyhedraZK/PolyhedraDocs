cb62dd72fbbf81790487f33e4b3a4c032d92f976
coda <coda@polyhedra.network>
2024-12-12 19:44:46 +0800

    update font

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -50,7 +50,7 @@ const config: Config = {
   ],
   stylesheets: [
     {
-      href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap",
+      href: "https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap",
       type: "text/css",
     },
   ],
