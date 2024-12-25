b9cbdcab554bd4c1ebe50d954269f1071d331318
Tiancheng Xie <tc@polyhedra.network>
2024-12-25 09:18:26 +0800

    fix latex

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -53,6 +53,13 @@ const config: Config = {
       href: "https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap",
       type: "text/css",
     },
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
   ],
   themeConfig: {
     metadata: [
